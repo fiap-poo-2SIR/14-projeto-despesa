8c9d1cb1e7197912bcd7ef868ef766f11e8066c8
Laboratório FIAP <marcos.selmini@uol.com.br>
2025-08-21 22:53:06 -0300

    classes Despesa e DespesaDAO

--- src/main/java/br/fiap/conexao/Conexao.java
+++ src/main/java/br/fiap/conexao/Conexao.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class Conexao {
     private final static String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-    private final static String user = "pf0015";
-    private final static String password = "fiap25";
+    private final static String user = "";
+    private final static String password = "";
 
     private Conexao() {}
 
